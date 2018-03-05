2e80f8926ff64b369bc594e36d7ffa77ca5efdbd
Jacob <jacob@jacobolofsson.se>
2018-03-05 13:49:46 +0100

    Completed address register

--- source/my_address_register.vhd
+++ source/my_address_register.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
 use work.my_package.all;
 
 entity my_address_register is
@@ -13,5 +14,19 @@ entity my_address_register is
 end entity;
 
 architecture rtl of my_address_register is
+   signal current_address : unsigned(REG_WIDTH-1 downto 0) := (others => '0');
 begin
+   process (CLK)
+   begin
+      if rising_edge(CLK) then
+         if INC_ADDRESS = '1' then
+            current_address <= current_address + 1;
+         elsif LD_ADDRESS = '1' then
+            current_address <= unsigned(NEW_ADDRESS);
+         end if;
+      end if;
+   end process;
+
+   OUT_ADDRESS <= std_logic_vector(current_address);
+
 end architecture;
